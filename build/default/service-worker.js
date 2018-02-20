b45574e52e8a7467f7deabc9d247fd54a8f83951
Elle Zadina <gaz16@case.edu>
2018-02-20 10:54:10 +0000

    add manifest and serviceworker

---
+++ build/default/service-worker.js
@@ -0,0 +1,56 @@
+var cacheName = 'haitiOrchardPWA-1';
+var dataCacheName = 'haitiOrchardData-1';
+var dataUrl = 'https://haiti-orchard.firebaseio.com/assignments/123456';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/src/haiti-orchard-app/haiti-orchard-app.html',
+  '/src/haiti-orchard-app/assignment-section.html',
+  '/src/haiti-orchard-app/assignment-cards.html'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if(key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  if (e.request.url.startsWith(dataUrl)) {
+    e.respondWith(
+      fetch(e.request)
+        .then(function(response) {
+          return caches.open(dataCacheName).then(function(cache) {
+            cache.put(e.request.url, response.clone());
+            console.log('[ServiceWorker] Fetched&Cached Data');
+            return response;
+          });
+        })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
